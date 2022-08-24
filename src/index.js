6f2fac5c3fe483dd7ae708ec09ae4e1365b30cf1
Lokeshwari Deepak <lokeshwarideepak@gmail.com>
2022-08-24 18:35:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -39,10 +39,6 @@ let forecastElement = document.querySelector("#forecast");
 let forecastHTML = `<div class="row">`;
 forecast.forEach(function(forecastDay, index) {
   if (index < 6) {
-
-  }
-}
-
 forecastHTML =
  forecastHTML +
 `
