4a292c99ab3d3f7243caccf3ddc0e248b21a66df
Lokeshwari Deepak <lokeshwarideepak@gmail.com>
2022-08-24 17:27:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -125,7 +125,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.date.1000);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
