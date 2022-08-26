85f5f0a170788c04d509b01b7b9d2bae414ef377
Lokeshwari Deepak <lokeshwarideepak@gmail.com>
2022-08-27 07:06:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "3b6843c21c9e3001b9979f4f906678e2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?
   lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&
   units=metric`;
   axios.get(apiUrl).then(displayForecast);
