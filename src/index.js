ab29196fe06757e2677ef92d7d1cd4fa53fb12de
Lokeshwari Deepak <lokeshwarideepak@gmail.com>
2022-08-27 19:55:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -118,12 +118,8 @@ function displayForecast(response) {
           width="44"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> ${Math.round(
-              forecastDay.temp.max
-            )}° </span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(
-              forecastDay.temp.min
-            )}° </span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
           </div>
         </div>
 `;
